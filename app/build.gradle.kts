553f3acbbc04b3872eec010b10e0825e2c166c09
Charles Beesley <79647115+mrbeag@users.noreply.github.com>
2026-09-04 14:32:06 +0100

    chore: bump phone release for Android Auto fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "uk.co.cbeesle1.homealarm"
         minSdk = 28
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
